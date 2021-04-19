04dea1f5437733a51d2891a1480b0467f7ddd82d
PedjaV <veljovic.predrag@gmail.com>
2021-04-19 21:07:05 +0200

    update

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var pageC = window.location.href
+// window.addEventListener("pageshow", function() {
+//     return pageC = window.location.href
+// })
+
+if(pageC.includes('single')== true){
+
+    console.log("radi sve te jebalo")
+}
+
+
+
+
+var ofo = [];
+var t = document.getElementsByClassName('ime')
+const p = window.addEventListener('click', function(e){
+    console.log(e)
+})
